977d934a5288cf355c4db7813e10783642351bb8
Ty Michael <tylerallenmichael@outlook.com>
2022-05-01 17:30:16 +0100

    Implemented quiz

--- quiz.js
+++ quiz.js
@@ -1,104 +1,34 @@
-var myQuestions = [
-    {
-        question: "1. What is the name of the killer in Scream?",
-        answers: {
-            a: 'Leatherface',
-            b: 'Jason Vorhees',
-            c: 'Michael Myers',
-            d: 'Ghostface'
-        },
-        correctAnswer: 'd'
-    },
-    {
-        question: "2. What is Sidney's last name?",
-        answers: {
-            a: 'Prescott',
-            b: 'Weary',
-            c: 'Strode',
-            d: 'Wayne'
-        },
-        correctAnswer: 'a'
-    },
-    {
-        question: "3. Who is the first victim in Scream?",
-        answers: {
-            a: 'Steve Orth',
-            b: 'Casey Becker',
-            c: 'Tatum Riley',
-            d: 'Randy Meeks'
-        },
-        correctAnswer: 'a'
-    },
-    {
-        question: "4. What is the killer's choice of weapon?",
-        answers: {
-            a: 'Machete',
-            b: 'Knife',
-            c: 'Gun',
-            d: 'Chainsaw'
-        },
-        correctAnswer: 'b'
-    },
+//Constants//
+const correctAnswers = ['C', 'A', 'B', 'B', 'D', 'A', 'C', 'B', 'C', 'D'];
+const quizForm = document.querySelector('.quizForm')
+const resultLabel = document.querySelector('#resultText');
 
-    {
-        question: "5. What object falls out of Billy's pocket that makes Sidney suspicious?",
-        answers: {
-            a: 'A knife',
-            b: 'A picture of her mom',
-            c: 'A gun',
-            d: 'A cell phone'
-        },
-        correctAnswer: 'd'
-    },
-    {
-        question: "6. What is the name of Sidney's best friend?",
-        answers: {
-            a: 'Gale',
-            b: 'Tatum',
-            c: 'Stu',
-            d: 'Randy'
-        },
-        correctAnswer: 'b'
-    },
-    {
-        question: "7. What is the killer's favourite question to ask his victims?",
-        answers: {
-            a: 'Do you want to die today?',
-            b: 'Are you home alone?',
-            c: 'What is your favourite scary movie?',
-            d: 'Do you get scared easily?'
-        },
-        correctAnswer: 'c'
-    },
-    {
-        question: "8. What is Gale's job?",
-        answers: {
-            a: 'Lawyer',
-            b: 'Reporter',
-            c: 'Nurse',
-            d: 'Sheriff'
-        },
-        correctAnswer: 'b'
-    },
+quizForm.addEventListener('submit', e => calculateResult(e))
+function calculateResult (e) {
+  e.preventDefault();
 
-    {
-        question: "9. What is the name of the fictional town the movie takes place?",
-        answers: {
-            a: 'Sunnydale',
-            b: 'Haddonfield',
-            c: 'Woodsboro',
-            d: 'Crystal Lake'
-        },
-        correctAnswer: 'c'
-    },
-    {
-        question: "10. Who is the final character to die in Scream?",
-        answers: {
-            a: 'Stu',
-            b: 'Dewey',
-            c: 'Kenny',
-            d: 'Billy'
-        },
-        correctAnswer: 'd'
-    }
-];
+//Takes user's answers and then calculates number of correct responses
+let inputAnswers = [quizForm.q1.value, quizForm.q2.value, quizForm.q3.value, quizForm.q4.value, quizForm.q5.value, quizForm.q6.value, quizForm.q7.value, quizForm.q8.value, quizForm.q9.value, quizForm.q10.value];
+let correctAnswersCount = 0;
+
+//Calculating number of correct answers user has given
+correctAnswers.forEach((correctAnswer, index) => {
+  if (correctAnswer === inputAnswers[index]) {
+    correctAnswersCount++;
+  }
+});
+
+//Calculating percentage of correct answers by using total length of array
+var correctPercentage = correctAnswersCount / correctAnswers.length * 100;
+
+  //Display percentage result
+if (correctPercentage > 60) {
+  resultLabel.textContent = "Congratulations! You did very well. You scored " + correctPercentage + "%."
+} else {
+  resultLabel.textContent = "You scored " + correctPercentage + "%. I'd suggest you try again or watch the movies."
+}
+
+console.log(resultText)
+//Reset form when complete
+  quizForm.reset();
+}
